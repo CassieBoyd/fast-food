cca7c38326729253c4c5f7ba6b5c04d9887f1993
Cassie Boyd <cassandra.bacon@gmail.com>
2019-10-20 20:46:37 -0500

    Completed comments

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,18 +6,25 @@
 // X4. Each meal object should have three properties: sandwichType, fries (true or false), and drinkSize.
 
 
-
+// "restaurant" is an object with four key/value pairs.
 const restaurant = {
     name: "Bob's Burgers",
+
+    // Key "placeOrder" has a method as its value. It takes in the parameter "meal" and pushes it to the "orders" array.
     placeOrder: function (meal) {
         this.orders.push(meal)
     },
+
+    // "orders" is a key with the initial value of an empty array.
     orders: [],
+
+    // Key "getAllOrders" has a method as its value. It returns the array associated with "orders".
     getAllOrders: function () {
         return this.orders
     }
 }
 
+// All combo meal variables are objects with key/value pairs. 
 const chickenComboMeal = {
     sandwichType: "chicken",
     fries: true,
@@ -37,6 +44,7 @@ const tofuComboMeal = {
 }
 
 // Place an order
+// Calling the placeOrder method on the "restaurant" object.
 restaurant.placeOrder(chickenComboMeal)
 restaurant.placeOrder(burgerComboMeal)
 restaurant.placeOrder(tofuComboMeal)
